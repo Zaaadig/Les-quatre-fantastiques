bea09692d3d50c2644aa26b74c3f9e34c9b4e1d6
Zaaadig <z_loyer@stu-lisaa.com>
2024-04-03 22:56:16 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -94,7 +94,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
-        print("test");
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
