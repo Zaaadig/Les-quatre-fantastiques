6a423ffed0b839e60dd4c4970adbbb836b2c37e3
Zaaadig <z_loyer@stu-lisaa.com>
2024-04-20 22:37:59 +0200

    Ajout touche shift pour courir

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -31,11 +31,16 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
+    float originalMoveSpeed;
+    bool speedDoubled = false;
+
     private void Start()
     {
+        originalMoveSpeed = moveSpeed;
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
+
 
     private void Update()
     {
@@ -60,7 +65,21 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        // Check if Shift key is pressed and speed is not already doubled
+        if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) && !speedDoubled)
+        {
+            // Double the moveSpeed if Shift key is pressed
+            moveSpeed *= 2f;
+            speedDoubled = true; // Set the flag to true to indicate speed has been doubled
+        }
+        else if (!(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && speedDoubled)
+        {
+            // Reset moveSpeed to its original value if Shift key is not pressed and speed was doubled before
+            moveSpeed /= 2f;
+            speedDoubled = false; // Reset the flag since speed has been reset
+        }
+
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
         {
             readyToJump = false;
 
@@ -69,6 +88,7 @@ public class PlayerMovement : MonoBehaviour
             Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
+
 
     private void MovePlayer()
     {
