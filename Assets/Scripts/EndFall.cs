3b63a87f02a9879095428fded84783822d7d0f7a
Zaaadig <z_loyer@stu-lisaa.com>
2024-05-07 15:55:29 +0200

    anim plateforme fin

--- Assets/Scripts/EndFall.cs
+++ Assets/Scripts/EndFall.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EndFall : MonoBehaviour
 {
     public GameObject poutre;
+    public GameObject poutre1;
+    public GameObject platform;
 
     private void OnTriggerEnter(Collider other)
     {
         poutre.GetComponent<Rigidbody>().useGravity = true;
         poutre.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None ;
+        poutre1.GetComponent<Rigidbody>().useGravity = true;
+        poutre1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+        platform.GetComponent<Animator>().Play("AnimEnd");
     }
 }
